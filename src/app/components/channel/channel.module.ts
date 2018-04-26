48b7e0bb121ac1c26192e3f97cea2c27f14527f3
Paulo Oliveira <paulooliveira@pecege.com>
2018-04-26 17:12:38 -0300

    channel

--- src/app/components/channel/channel.module.ts
+++ src/app/components/channel/channel.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import {AgGridModule} from "ag-grid-angular/main";
 import { SharedModule } from '../shared/shared.module';
 import { ChannelService } from '../../services/channel/channel.service';
+import { ChannelComponent } from './channel.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    FormsModule
+    FormsModule,
+    AgGridModule.withComponents([])     
   ],
-  providers: [ ChannelService ]
+  declarations: [ 
+    ChannelComponent,    
+  ],
+  exports: [ ChannelComponent ],
+  providers: [ ChannelService ],  
 })
-export class MachineModule { }
+export class ChannelModule { }
