f6ea54d8569bc45583c0e8f788cf25ea060f7220
Paulo Oliveira <paulooliveira@pecege.com>
2018-11-27 21:24:42 -0200

    atualização da lista de pausas no dash

--- src/app/models/machine.pause.list.ts
+++ src/app/models/machine.pause.list.ts
@@ -3,11 +3,16 @@ import { MachinePause } from "./machine.pause";
 
 class Pauses {
   id: number;
-  mc_cd: string;
-  name: string;
+  channel_id: number;
   date_ref: string;
-  justification: string;
-  inserted_at: string;
+  date_ref_format: string;
+  machine_code: string;
+  machine_name: string;
+  pause: string;
+  pause_id: number;
+  pause_reason: string;
+  pause_time: string;
+  row_num: number;
 }
 
 export class MachinePauseList extends BaseModel {
